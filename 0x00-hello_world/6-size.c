07b561ab1584455987df58b6b56d3d082b8d9386
Pascal <gregchris2016@gmail.com>
2023-09-07 13:57:37 -0700

    C Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int c;
 long long int d;
 float e;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-printf("Size_of a float: %lu bytes(s)\n", (unsigned long)sizeof(e));
+printf("Size of a char: %u byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %u byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %u bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %u bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size_of a float: %u bytes(s)\n", (unsigned long)sizeof(e));
 return (0);
 }
