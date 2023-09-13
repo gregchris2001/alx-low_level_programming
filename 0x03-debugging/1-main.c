8c353991baf71ea5ff4b3aee1d21474b44d025ba
Pascal <gregchris2016@gmail.com>
2023-09-13 16:16:36 -0700

    C - Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,9 +7,7 @@
 int main(void)
 {
 	int i;
-
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 
 	/*while (i < 10)*/
@@ -18,6 +16,5 @@ int main(void)
 	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
