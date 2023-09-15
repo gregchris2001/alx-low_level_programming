7170e24e6cc816ea433df12997cee080541b1d6a
Pascal <gregchris2016@gmail.com>
2023-09-14 18:41:05 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
+ * _isupper - Check main
+ * @c: An input character
+ * Description: function that checks for uppercase character
+ * Return: 1 if is uppercase or 0 if is lowercase
  */
-
 int _isupper(int c)
 {
-if (c > 'A' && c < 'Z')
-	return (1);
-else
-	return (0);
-
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
