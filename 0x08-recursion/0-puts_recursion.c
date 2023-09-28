b7df471bb616e386c434c55752da0130495ac75b
Pascal <gregchris2016@gmail.com>
2023-09-28 07:58:40 -0700

    0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,11 @@
  * Return: Always 0 (Success)
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s > '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
+       		_putchar(*s);
 	}
 }
